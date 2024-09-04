c6e6974f58d7b0d3d4dd215646fe4f56afa1bffa
Karim Q. <kareem.qiblawi@gmail.com>
2024-09-04 18:35:31 +0300

    Fixed

--- JawakerBot.py
+++ JawakerBot.py
@@ -1,42 +1,65 @@
 '''
 Author: Karim Q.
-Date: 11/2/21
+Original Date: 11/2/21
+Update Date: 4/9/24
+
+Update Note:
+Probably should've been done with classes, but well if it ain't broke dont fix it. I only updated the broken parts and slightly improved readability.
 '''
-import numpy as np
-from PIL import Image, ImageGrab
 from pynput.mouse import Button, Controller
-import cv2
-from matplotlib import pyplot as plt
-import time
+# from matplotlib import pyplot as plt
+import pyscreenshot as ImageGrab
+from PIL import Image
+import numpy as np
 import random
+import time
+import cv2
+import os
 
-Mouse = Controller()
+MOUSE_CONTROL = Controller()
 
-Cards = ['s2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 'sJ', 'sQ', 'sK', 'sA',
+DECK_CARDS = ['s2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 'sJ', 'sQ', 'sK', 'sA',
         'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9', 'd10', 'dJ', 'dQ', 'dK', 'dA',
         'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10', 'cJ', 'cQ', 'cK', 'cA',
         'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9', 'h10', 'hJ', 'hQ', 'hK', 'hA']
 
-Hierachy = {"c":['c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10', 'cJ', 'cQ', 'cK', 'cA', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9', 'h10', 'hJ', 'hQ', 'hK', 'hA'],
+HIERACHY = {"c":['c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10', 'cJ', 'cQ', 'cK', 'cA', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9', 'h10', 'hJ', 'hQ', 'hK', 'hA'],
             "d":['d2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9', 'd10', 'dJ', 'dQ', 'dK', 'dA', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9', 'h10', 'hJ', 'hQ', 'hK', 'hA'],
             "s":['s2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 'sJ', 'sQ', 'sK', 'sA', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9', 'h10', 'hJ', 'hQ', 'hK', 'hA'],
             "h":['h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9', 'h10', 'hJ', 'hQ', 'hK', 'hA']}
 
-Bets = {2:[(404, 485)],
-        3:[(455, 482), (404, 485)],
-        4:[(503, 487), (455, 482)],
-        5:[(560, 486), (503, 487)],
-        6:[(403, 538), (560, 486)]}
+BET_POSITIONS = {2:[(404, 520)],
+        3:[(455, 517), (404, 520)],
+        4:[(503, 522), (455, 517)],
+        5:[(560, 521), (503, 522)],
+        6:[(403, 573), (560, 521)]}
+
+SUITS = ["Heart", "Spade", "Diamond", "Club"]
+VALUES = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+DIRS = {"HAND_CARD_SUITS":"./Assets/Hand/Suits", 
+        "HAND_CARD_VALUES":"./Assets/Hand/Values",
+        "TABLE_CARD_SUITS":"./Assets/Table/Suits", 
+        "TABLE_CARD_VALUES":"./Assets/Table/Values",
+        "TEST_FOR_2":"./Assets/General/Test2_V2.png",
+        "TEST_FOR_3":"./Assets/General/Test3_V2.png",
+        "TEMP":"./Temp/tmp.png"}
 
 PlayedCards = []
 HandCards = []
 CompleteFail = False
+MateCompleteFail = False
 Choose = False
 Plays = False
 
 def Play(Pos, Margin):
-    Mouse.position = (Margin[0]+Pos[0], Margin[1]+Pos[1])
-    Mouse.click(Button.left, 2)
+    MOUSE_CONTROL.position = (Margin[0]+Pos[0], Margin[1]+Pos[1])
+    # MOUSE_CONTROL.click(Button.left, 2)
+    MOUSE_CONTROL.press(Button.left)
+    MOUSE_CONTROL.release(Button.left)
+    time.sleep(0.1)
+    MOUSE_CONTROL.press(Button.left)
+    MOUSE_CONTROL.release(Button.left)
+
 
 def Find(Crd, Img):
     OriImg = cv2.imread(Img)
@@ -45,74 +68,82 @@ def Find(Crd, Img):
     Pos = None
     # w, h = Card.shape[::-1]
     Match = cv2.matchTemplate(BnWImg, Card, cv2.TM_CCOEFF_NORMED)
-    Loc = np.where( Match >= 0.95 )
-    for pt in zip(*Loc[::-1]):
-        Pos = pt
-        break
+    Loc = np.where(Match >= 0.9)
+    return [pt for pt in zip(*Loc[::-1])]
+    #     Pos = pt
+    #     break
         # cv2.rectangle(OriImg, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
     # cv2.imwrite('res.png',OriImg)
-    return Pos
 
 def CheckTurn():
-    TurnI = ImageGrab.grab((442,991,456,1002))
-    TurnI.save("Turn.png")
-    Img = Image.open("Turn.png")
+    TurnI = ImageGrab.grab((310,953,317,960))
+    TurnI.save(DIRS["TEMP"])
+    Img = Image.open(DIRS["TEMP"])
     Pixels = Img.load()
-    return CheckColor(Pixels[0,0])
+    print("Turn Circle Pixel Color: ", Pixels[4,4])
+    return CheckColor(Pixels[4,4])
 
 def CheckComplete():
-    CompI = ImageGrab.grab((540,698,542,701))
-    CompI.save("Comp.png")
-    Img = Image.open("Comp.png")
+    CompI = ImageGrab.grab((600,1005,605,1010))
+    CompI.save(DIRS["TEMP"])
+    Img = Image.open(DIRS["TEMP"])
     Pixels = Img.load()
-    return CheckColor(Pixels[0,0])
+    return CheckColor(Pixels[2,2])
 
 def CheckMateComplete():
-    CompI = ImageGrab.grab((390,268,397,272))
-    CompI.save("CompMate.png")
-    Img = Image.open("CompMate.png")
+    CompI = ImageGrab.grab((475,380,480,385))
+    CompI.save(DIRS["TEMP"])
+    Img = Image.open(DIRS["TEMP"])
     Pixels = Img.load()
-    return CheckColor(Pixels[0,0])
+    return CheckColor(Pixels[2,2])
 
 def CheckColor(RGB):
-    if (245 < RGB[0] < 249) and (181 < RGB[1] < 186) and (18 < RGB[2] < 23):
+    if (230 <= RGB[0] <= 250) and (190 <= RGB[1] <= 200) and (60 <= RGB[2] <= 70):
         return "Y"
-    elif (73 < RGB[0] < 78) and (189 < RGB[1] < 193) and (103 < RGB[2] < 108):
+    elif (40 <= RGB[0] <= 50) and (175 <= RGB[1] <= 185) and (55 <= RGB[2] <= 65):
         return "G"
-    elif (227 < RGB[0] < 232) and (77 < RGB[1] < 83) and (84 < RGB[2] < 89):
+    elif (115 <= RGB[0] <= 125) and (6 <= RGB[1] <= 16) and (18 <= RGB[2] <= 28):
         return "R"
-    elif (57 < RGB[0] < 72) and (57 < RGB[1] < 72) and (57 < RGB[2] < 72):
+    elif (0 <= RGB[0] <= 10) and (0 <= RGB[1] <= 10) and (0 <= RGB[2] <= 10):
         return "B"
     else:
         return "U"
 
 def GetMyCards():
     global PlayedCards
-    HandI = ImageGrab.grab((255,728,702,881))
-    HandI.save("Hand.png")
+    HandI = ImageGrab.grab((255,800,700,880))
+    HandI.save(DIRS["TEMP"])
     Hand = {}
-    for Card in Cards:
-        P = Find(f'Hand/{Card}.png', "Hand.png")
-        if P:
-            Hand[Card] = P
-            PlayedCards.append(Card)
-    print(f'Hand: {Hand}')
+    tempVals = {v:Find(os.path.join(DIRS["HAND_CARD_VALUES"], f"{v}.png"), DIRS["TEMP"]) for v in VALUES}
+    tempSuits = {s:Find(os.path.join(DIRS["HAND_CARD_SUITS"], f"{s}.png"), DIRS["TEMP"]) for s in SUITS}
+    for suit, posS in tempSuits.items():
+        for xS, _ in posS:
+            for val, posV in tempVals.items():
+                for pos in posV:
+                    if abs(xS-pos[0]) <= 2:
+                        Card = suit[0].lower()+val
+                        Hand[Card] = pos
+                        PlayedCards.append(Card)
     return Hand
 
 def GetPlayed():
     global PlayedCards
-    TableI = ImageGrab.grab((388,379,572,649))
-    TableI.save("Table.png")
+    TableI = ImageGrab.grab((390,420,570,620))
+    TableI.save(DIRS["TEMP"])
     TableD = {}
     Table = []
-    for Card in Cards:
-        P = Find(f'Table/{Card}.png', "Table.png")
-        if P:
-            TableD[P[0]] = Card
-            PlayedCards.append(Card)
+    tempVals = {v:Find(os.path.join(DIRS["TABLE_CARD_VALUES"], f"{v}.png"), DIRS["TEMP"]) for v in VALUES}
+    tempSuits = {s:Find(os.path.join(DIRS["TABLE_CARD_SUITS"], f"{s}.png"), DIRS["TEMP"]) for s in SUITS}
+    for suit, posS in tempSuits.items():
+        for xS, _ in posS:
+            for val, posV in tempVals.items():
+                for xV, _ in posV:
+                    if abs(xS-xV) <= 2:
+                        Card = suit[0].lower()+val
+                        TableD[xV] = Card
+                        PlayedCards.append(Card)
     for i in sorted(TableD):
         Table.append(TableD[i])
-    print(f'Table: {Table}')
     return Table
 
 def BestPlay(Hand, Table):
@@ -132,14 +163,14 @@ def BestPlay(Hand, Table):
         Hand = CardCleaner(Hand, CardType)
         Available = (Hand[0][0] == CardType)
         print(f'Available?: {Available}')
-        WantedHierachy = Hierachy[CardType]
-        NunsHierachy = Hierachy["h"]
+        WantedHierachy = HIERACHY[CardType]
+        NunsHierachy = HIERACHY["h"]
         AvailableHierachy = WantedHierachy[:13]
         ImportantCards = [C for C in Table if C in WantedHierachy]
         ImportantCards.sort(key = lambda x: WantedHierachy.index(x))
         StrongestCard = ImportantCards[-1]
         print(f'Strongest: {StrongestCard}')
-        Hand.sort(key = lambda x: Cards.index(x))
+        Hand.sort(key = lambda x: DECK_CARDS.index(x))
         ForTeam = True if Table.index(StrongestCard) == 1 else False
         print(f'Strongest For Mate?: {ForTeam}')
         if Available:
@@ -185,15 +216,15 @@ def BestPlay(Hand, Table):
         Hand, H = CardSplitter(Hand)
         Playable = []
         for i in AKs:
-            HiS = Hierachy[i[0]][:13]
+            HiS = HIERACHY[i[0]][:13]
             if set(HiS[HiS.index(i)+1:]).issubset(PlayedCards):
                 Playable.append(i)
         return random.choice(Playable) if Playable else random.choice(Hand) if Hand else H[0]
 
 def DetectPhase():
-    Number = ImageGrab.grab((373, 459, 427, 506))
-    Number.save("NumberToTest.png")
-    Phaser = 0 if Find("TestIf2.png", "NumberToTest.png") else 1 if Find("TestIf3.png", "NumberToTest.png") else -1
+    Number = ImageGrab.grab((393, 520, 427, 547))
+    Number.save(DIRS["TEMP"])
+    Phaser = 0 if Find(DIRS["TEST_FOR_2"], DIRS["TEMP"]) else 1 if Find(DIRS["TEST_FOR_3"], DIRS["TEMP"]) else -1
     print(f'Playing Phase: {Phaser}')
     return Phaser
 
@@ -215,7 +246,7 @@ def BestBet(Hand):
     PfD += 1 if "dK" in Cs else 0
     PfS += 1 if "sK" in Cs else 0
 
-    for i in reversed(Hierachy["h"]): 
+    for i in reversed(HIERACHY["h"]): 
         if i not in Hs: break    
         PfH+=1
 
@@ -223,7 +254,7 @@ def BestBet(Hand):
     print(f'Best Bet: {BetMake}')
     return BetMake
 
-input("Enter to Start...  ")
+# print(CheckComplete())
 
 while True:
     if CheckTurn() == "Y":
@@ -235,12 +266,14 @@ while True:
                 HandCards = GetMyCards()
                 Bet = BestBet(HandCards.keys())
                 Bet = Threshold if Bet<Threshold else 6 if Bet>6 else Bet
-                Play(Bets[Bet][Phase], (0,0))
+                Play(BET_POSITIONS[Bet][Phase], (0,0))
                 Choose = False
                 print("-"*100)
             time.sleep(1.5)
             if CheckTurn() == "Y":
-                Plays = DetectPhase() == -1 and CheckComplete() == "B"
+                KPds = CheckComplete()
+                print(KPds)
+                Plays = DetectPhase() == -1 and KPds == "B"
                 Choose = True
 
     while len(HandCards)-1 != 0 and Plays:
@@ -254,6 +287,6 @@ while True:
             print(f'Mate Complete or Fail?: {MateCompleteFail}')
             HandCards = GetMyCards()
             TableCards = GetPlayed()
-            Play(HandCards[BestPlay(list(HandCards.keys()), TableCards)], (260, 732))
+            Play(HandCards[BestPlay(list(HandCards.keys()), TableCards)], (260, 832))
             print("-"*100)
     Plays = False
